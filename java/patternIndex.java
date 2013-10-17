6fd3d036bbf90c0e0ae51a7e2e2bc2bdfc4505bf
aaraya1516@gmail.com <aaraya1516@gmail.com>
2013-10-16 22:48:39 -0500

    start java attempts

---
+++ java/patternIndex.java
@@ -0,0 +1,23 @@
+#
+#
+# Pattern Matching
+# Outputs character index start counts at 0
+#
+#
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class patternIndex {
+	
+	public static void main(String[] args) {
+		File 
+		String pattern "CTTGATCAT"
+		if(F)
+		System.out.println("");
+	}
+}
