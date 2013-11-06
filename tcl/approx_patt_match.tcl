51570b923f1d1aaca843cf41cce7ae15732958e7
aaraya1516@gmail.com <aaraya1516@gmail.com>
2013-11-05 22:51:58 -0600

    approximate mattern matching start

---
+++ tcl/approx_patt_match.tcl
@@ -0,0 +1,99 @@
+#Approximate Pattern Matching Problem: Find all approximate occurrences of a pattern in a string.
+#     Input: Two strings Pattern and Text along with an integer d.
+#     Output: All positions where Pattern appears in Text with at most d mismatches.
+#
+#CODE CHALLENGE: Solve the Approximate Pattern Matching Problem
+#
+#Sample Input:
+#     ATTCTGGA
+#     CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT
+#     3
+set runtimeStart [clock seconds]
+# get string we are analyzing from genom
+if {[file exists ../aprx_genome.txt]} {
+	set genomefile [open ../genome.txt r]
+	set gnomestr [read $genomefile]
+	close $genomefile
+	puts "Genome File Found!"
+} else {
+	set gnomestr "CGGACTCGACAGATGTGAAGAACGACAATGTGAAGACTCGACACGACAGAGTGAAGAGAAGAGGAAACATTGTAA"
+}
+set gnmstrL [string length $gnomestr]
+# Quantify permutations if allowed
+set t 3
+#What are we matching from?
+set mtx_base "ATTCTGGA"
+set mtx_l [string length $mtx_base]
+	set strRange $mtx_base
+	set gnmstrLmod [expr $t*[string length $gnomestr]]
+	set kMod 0
+	set k 0
+
+if {$mtx_base == ""} {
+	# k = k-mer length;  codon size we are looking at
+	set k 9
+	set kMod 0
+	set gnmstrLmod [string length $gnomestr]-$k
+}
+
+#Are we looking in a frame?
+# L = length of frame
+if {0} {
+	set L 500
+} {
+	set L $gnmstrL
+}
+
+set ::rangeList ""
+#
+# Start Sorting through string
+#
+for {set i 0} { $i < $gnmstrLmod} {incr i} {
+	#Do we have a mtx_base given?
+	if {$mtx_base == ""} {
+		# This gets the $k length character string desired
+		set strRange [string range $gnomestr $i [expr $i + $kMod]]
+		# count all matching strings
+		set regexpCount [regexp -all $strRange [string range $gnomestr $i+1 $i+$L]]
+		#puts "$strRange:$regexpCount"
+		if {$regexpCount >= $t && [string length $strRange] == $k && [lsearch $::rangeList $strRange] == -1} {
+			lappend ::rangeList $strRange
+			#puts "$strRange:$regexpCount"
+		}
+	} else {
+		#
+		#Approximate Pattern Matching
+		#
+		puts $i
+		set second 0
+		set third 0
+		set fourth 0
+		for {set inc 0} {$inc <= [expr $mtx_l-[expr $t-1]]} {incr inc} {
+			if {$second == 0} {
+				set mtx_base [string replace $mtx_base $inc $inc "."]
+				incr second
+			} else {
+				set mtx_base [string replace $mtx_base 0 0 "."]
+			}
+			for {set in 1} {$in <= [expr $mtx_l-[expr $t-1]]} {incr in} {
+				set mtx_base [string replace $mtx_base $in $in "."]
+			}
+			puts $mtx_base
+			#count mathing strings
+			set matchCount [regexp -all $gnomestr $mtx_base]
+		}		
+	}
+
+}
+
+set runtimeEnd [clock seconds]
+
+puts "Length: $gnmstrL
+second: $second
+third: $third
+fourth: $fourth
+rangeList: $::rangeList
+rangeList Length: [llength $::rangeList]
+runtimeStart: $runtimeStart
+runtimeEnd: $runtimeEnd
+runtime: [expr $runtimeEnd - $runtimeStart]"
