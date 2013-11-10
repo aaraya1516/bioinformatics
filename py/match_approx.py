78eb836a9e700d703151d10524a6e7e8fd1b3531
aaraya1516@gmail.com <aaraya1516@gmail.com>
2013-11-10 17:49:15 -0600

    start Frequent Words with Mismatches Problem, touch up approx match

--- py/match_approx.py
+++ py/match_approx.py
@@ -1,21 +1,25 @@
-#Approximate Pattern Matching Problem: Find all approximate occurrences of a pattern in a string.
-#     Input: Two strings Pattern and Text along with an integer d.
-#     Output: All positions where Pattern appears in Text with at most d mismatches.
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 17:39:28 2013
+
+@author: alejandro
+"""
+#Frequent Words with Mismatches Problem: Find the most frequent k-mers with mismatches in a string.
+#     Input: A string Text as well as integers k and d. (You may assume k ≤ 12 and d ≤ 3.)
+#     Output: All most frequent k-mers with up to d mismatches in Text.
 #
-#CODE CHALLENGE: Solve the Approximate Pattern Matching Problem
+#CODE CHALLENGE: Solve the Frequent Words with Mismatches Problem.
 #
 #Sample Input:
-#     ATTCTGGA
-#     CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT
-#     3
-
+#     ACGTTGCATGTCGCATGATGCATGAGAGCT 4 1
+#Sample Output:
+#     GATG ATGC ATGT
 
 from os import path, access, R_OK  # W_OK for write permission.
-#import time
-import regex
+import regex, time
 
-PATH='../approx.txt'
-useFile = 1;
+PATH='../matching_approx.txt'
+useFile = 2;
 if path.isfile(PATH) and access(PATH, R_OK) and useFile == 1:
     print "Genome File Found!";
     #useGenomeFile = raw_input('To use file press 1:');
@@ -31,14 +35,36 @@ if path.isfile(PATH) and access(PATH, R_OK) and useFile == 1:
         genomeFile.close();
     #print "String:", genomeStr
 else:
-    genomeStr = "CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT";
+    genomeStr = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
 
-# What are the possible combos of base^t
-base= "AGGTACAT";#"ATTCTGGA"
+# k-mer length
+k = 4;
+# Number of mismatches
+d = 1;
 output="";
 indG="";
 dblsMatch=[];
 indList=[];
+i = 0;
+epoch_timeStart = int(time.time())
+rangeList = [];
+
+strLength = len(genomeStr)
+print "Starting Loop"
+while (i <= strLength-k):
+    # count 1 plus k-mer length
+    endRange = i+k
+    #value of string 
+    strRange = genomeStr[i:endRange]
+
+    if len(strRange) == k and genomeStr.count(strRange, i, i+L) >= t and strRange not in rangeList:
+        rangeList.append(strRange)
+        #concatString=concatString+" "+strRange
+        #print "String: "+strRange
+        #print "Count: "+str(genomeStr.count(strRange, i, i+L))
+
+    i=i+1
+    
 m=regex.findall("(AGGTACAT){s<=5}", genomeStr, overlapped=True)
 #m=regex.findall()
 #print m
